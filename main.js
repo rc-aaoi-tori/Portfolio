3f81fe94874eb539c4a6f8e1084d30aaad8a74b3
Reshmi Chowdhury <reshmic108@gmail.com>
2024-11-09 16:04:05 +0530

    jscript file modified for resize off tab pan

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-$(function(){
-
-    let $hamburgerWrapper = $('.hamburger-wrapper');
-    let $tabpanelMenus = $('.tab-tab');
-    let $tabpaneToggler = $('.tabpane-toggler');
-
-    $hamburgerWrapper.on('click', function(e){
-        e.preventDefault();
-        $(this).children('.hamburger-menu').toggleClass('animate');
-
-        $tabpanelMenus.slideToggle(1000);
-    });
-
-    $tabpaneToggler.on('click', function(){
-        $tabpanelMenus.slideToggle(1000);
-        $hamburgerWrapper.children('.hamburger-menu').toggleClass('animate');
-    });
-
-});
+$(function () {
+    function hamBurgerMenu() {
+      let screenWidth = screen.width;
+      let $hamburgerWrapper = $(".hamburger-wrapper");
+      let $tabpanelMenus = $(".tab-tab");
+      let $tabpaneToggler = $(".tabpane-toggler");
+  
+      // Following js will execute for mobile only.
+      if (screenWidth < 768) {
+          console.log("We're in mobile");
+        $hamburgerWrapper.on("click", function (e) {
+          e.preventDefault();
+          $(this).children(".hamburger-menu").toggleClass("animate");
+  
+          $tabpanelMenus.slideToggle(1000);
+        });
+  
+        $tabpaneToggler.on("click", function () {
+          $tabpanelMenus.slideToggle(1000);
+          $hamburgerWrapper.children(".hamburger-menu").toggleClass("animate");
+        });
+      }
+    }
+  
+    hamBurgerMenu();
+    window.addEventListener('resize', hamBurgerMenu);
+  });
